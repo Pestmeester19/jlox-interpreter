25a20aa240cfde77358e8b8fd2e6a17a693fe761
Pestmeester <2941950-Pest@users.noreply.gitlab.com>
2021-11-20 23:13:47 +0000

    Ready, Set, Gooo!

---
+++ src/com/pestmeester/lox/Lox.java
@@ -0,0 +1,51 @@
+package com.pestmeester.lox;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class Lox {
+    public static void main(String[] args) throws IOException {
+        if(args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+        } else if(args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader((System.in));
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;) {
+            System.out.println("> ");
+            String line = reader.readLine();
+            if(line == null) {
+                break;
+            }
+            run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for(Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+}
