6001e61136c8fa17b8e6751df6c79caed60b3151
Pestmeester <2941950-Pest@users.noreply.gitlab.com>
2021-11-21 23:23:06 +0000

    Working on operators lexemes

--- src/com/pestmeester/lox/Scanner.java
+++ src/com/pestmeester/lox/Scanner.java
@@ -28,27 +28,69 @@ public class Scanner {
     private void scanToken() {
         char c = advance();
         switch(c) {
-            case '(': addToken(TokenType.LEFT_PAREN);
-            break;
-            case ')': addToken(TokenType.RIGHT_PAREN);
-            break;
-            case '{': addToken(TokenType.LEFT_BRACE);
-            break;
-            case '}': addToken(TokenType.RIGHT_BRACE);
-            break;
-            case ',': addToken(TokenType.COMMA);
-            break;
-            case '.': addToken(TokenType.DOT);
-            break;
-            case '-': addToken(TokenType.MINUS);
-            break;
-            case '+': addToken(TokenType.PLUS);
-            break;
-            case ';': addToken(TokenType.SEMICOLON);
-            break;
-            case '*': addToken(TokenType.STAR);
-            break;
+            case '(':
+                addToken(TokenType.LEFT_PAREN);
+                break;
+            case ')':
+                addToken(TokenType.RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(TokenType.LEFT_BRACE);
+                break;
+            case '}':
+                addToken(TokenType.RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(TokenType.COMMA);
+                break;
+            case '.':
+                addToken(TokenType.DOT);
+                break;
+            case '-':
+                addToken(TokenType.MINUS);
+                break;
+            case '+':
+                addToken(TokenType.PLUS);
+                break;
+            case ';':
+                addToken(TokenType.SEMICOLON);
+                break;
+            case '*':
+                addToken(TokenType.STAR);
+                break;
+
+            case '!':
+                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+
+            case '<':
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+
+            case '>':
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+                break;
+
+
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
         }
+    }
+
+    private boolean match(char expected) {
+        if(isAtEnd()) {
+            return false;
+        }
+        if(source.charAt(current) != expected) {
+            return false;
+        }
+
+        current++;
+        return true;
     }
 
     private boolean isAtEnd() {
